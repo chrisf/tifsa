dbe2ffead07a25384520e26ec2025b5302a5254f
Chris Fitzmorris <cfitz93@gmail.com>
2017-01-27 13:49:32 -0600

    Test connecting to azure sql database

--- src/main/java/org/jsack/tifsa/Main.java
+++ src/main/java/org/jsack/tifsa/Main.java
@@ -5,11 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jsack.tifsa.Sql.TestConnect;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        TestConnect test = new TestConnect();
+
         Parent root = FXMLLoader.load(getClass().getResource("/main_window.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
