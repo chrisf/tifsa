6f957eb7981ddc106349b7a6987bd26fc702d65b
Aaron R <aaronrubesh6@gmail.com>
2017-02-11 00:31:27 -0600

    Added SqlConnect class with working connection to Google SQL\nAdded SqlConnect Test\nHave fun!

--- src/test/SqlConnectTest.groovy
+++ src/test/SqlConnectTest.groovy
@@ -1,6 +1,11 @@
+import org.jsack.tifsa.Sql.SqlConnect
 
 /**
  * Created by aaron on 2/11/17.
  */
-class SqlConnectTest {
+class SqlConnectTest extends GroovyTestCase {
+    void testSqlConnectionClass() {
+        SqlConnect sqlConnect = new SqlConnect(SqlConnect.SQL_SERVER_SELECTION.MySql)
+        assertTrue(true)
+    }
 }
