0fbf1374ba3e9038c412518112c36d5cb7ed84f1
Aaron R <aaronrubesh6@gmail.com>
2017-03-13 17:53:43 -0500

    changed context, added tests

---
+++ src/main/java/org/jsack/tifsa/CustomerWrapper.java
@@ -0,0 +1,7 @@
+package org.jsack.tifsa;
+
+/**
+ * Created by aaron on 3/13/17.
+ */
+public class CustomerWrapper {
+}
--- src/test/org/jsack/tifsa/core/CustomerTest.java
+++ src/test/org/jsack/tifsa/core/CustomerTest.java
@@ -1,8 +1,18 @@
 package org.jsack.tifsa.core;
 
+import org.jsack.tifsa.core.models.Customer;
+
 /**
  * Created by aaron on 3/1/17.
  */
-public class CustomerTest {
 
+
+
+public class CustomerTest {
+    public void testStandardCustomer(){
+        Customer c = new Customer("Aaron", "D", "Rubesh", "Aaron", "D",
+                "Rubesh", 0, "123 st", "123", "123", "st2");
+
+
+    }
 }
