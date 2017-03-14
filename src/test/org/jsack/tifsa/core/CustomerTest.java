29cd410fa1d3f8630e7caa0f1a7aafafd2a9749b
Aaron R <aaronrubesh6@gmail.com>
2017-03-13 21:33:15 -0500

    Changed the pom.xml, Created CustomerTest, idk these changes didn't get committed earlier.

--- src/test/org/jsack/tifsa/core/CustomerTest.java
+++ src/test/org/jsack/tifsa/core/CustomerTest.java
@@ -1,18 +1,34 @@
 package org.jsack.tifsa.core;
 
-import org.jsack.tifsa.core.models.Customer;
+import org.jsack.tifsa.DBModelWrappers.CustomerWrapper;
+import org.jsack.tifsa.DBModels.Customer;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 /**
  * Created by aaron on 3/1/17.
  */
 
 
-
 public class CustomerTest {
-    public void testStandardCustomer(){
-        Customer c = new Customer("Aaron", "D", "Rubesh", "Aaron", "D",
-                "Rubesh", 0, "123 st", "123", "123", "st2");
+    @Test
+    public void testStandardCustomer() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate template = new JdbcTemplate((javax.sql.DataSource) context.getBean("dataSource"));
+        String sql = "SELECT * FROM Customer";
+        List<Customer> customers = template.query(sql, new CustomerWrapper());
 
-
+        if (customers.size() > 0) {
+            for (Customer c : customers) {
+                System.out.println(c.getCustomerFirst());
+            }
+            assert (true);
+        } else {
+            assert (false);
+        }
     }
 }
