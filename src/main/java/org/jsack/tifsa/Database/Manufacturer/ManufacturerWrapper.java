38c26e761c9fcd1ce67c4215c6dfae0f8be0c650
jayjay-a <agno.julius@gmail.com>
2017-03-21 13:55:09 -0500

    add manufacturer model

---
+++ src/main/java/org/jsack/tifsa/Database/Manufacturer/ManufacturerWrapper.java
@@ -0,0 +1,18 @@
+package org.jsack.tifsa.Database.Manufacturer;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by julius on 3/21/2017.
+ */
+public class ManufacturerWrapper implements org.springframework.jdbc.core.RowMapper<Manufacturer>{
+    public Manufacturer mapRow(ResultSet rs, int rowNum) throws SQLException{
+        Manufacturer manufacturer = new Manufacturer();
+
+        manufacturer.setManufacturerID(rs.getInt("countryID"));
+        manufacturer.setManufacturerName(rs.getString("ManufacturerName"));
+
+        return manufacturer;
+    }
+}
