f586972f58ac02d3ce0f8a5d7c8bd5770f937001
guptanil14 <uhanilgupta@gmail.com>
2017-03-22 19:02:40 -0500

    added model -Employee role and employeeStatus

---
+++ src/main/java/org/jsack/tifsa/Database/EmployeerRole/EmployeeRole.java
@@ -0,0 +1,29 @@
+package org.jsack.tifsa.Database.EmployeerRole;
+
+/**
+ * Created by anil gupta on 3/21/2017.
+ */
+public class EmployeeRole {
+
+    private long employeeID;
+    private long employeeTypeID;
+
+    public long getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(long employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public long getEmployeeTypeID() {
+        return employeeTypeID;
+    }
+
+    public void setEmployeeTypeID(long employeeTypeID) {
+        this.employeeTypeID = employeeTypeID;
+    }
+
+
+}
+
