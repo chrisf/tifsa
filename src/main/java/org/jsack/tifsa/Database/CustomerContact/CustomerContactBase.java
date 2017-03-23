1cc17d4d6dccf71a784f017049f7b9776d7c2f89
Aaron R <aaronrubesh6@gmail.com>
2017-03-22 22:43:46 -0500

    Finished baseline classes for CustomerContact. Will update with new delete method when it's available.

--- src/main/java/org/jsack/tifsa/Database/CustomerContact/CustomerContactBase.java
+++ src/main/java/org/jsack/tifsa/Database/CustomerContact/CustomerContactBase.java
@@ -1,7 +1,17 @@
 package org.jsack.tifsa.Database.CustomerContact;
 
+import org.jsack.tifsa.Database.ModelBase;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by aaron on 3/22/17.
  */
-public class CustomerContactBase {
+public abstract class CustomerContactBase extends ModelBase {
+    public abstract List<CustomerContact> selectAll();
+    public abstract CustomerContact selecyById(long id);
+    public abstract long create(CustomerContact m);
+    public abstract int update(long id, Map<String, Object> attributes);
+    public abstract int delete(long id);
 }
--- src/main/java/org/jsack/tifsa/Database/CustomerContact/CustomerContactDAO.java
+++ src/main/java/org/jsack/tifsa/Database/CustomerContact/CustomerContactDAO.java
@@ -1,7 +1,43 @@
 package org.jsack.tifsa.Database.CustomerContact;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by aaron on 3/22/17.
  */
-public class CustomerContactDAO {
+public class CustomerContactDAO extends CustomerContactBase {
+    @Override
+    public List<CustomerContact> selectAll() {
+        String sql = "SELECT * FROM CustomerContact";
+        return getTemplate().query(sql, new CustomerContactWrapper());
+    }
+
+    @Override
+    public CustomerContact selecyById(long id) {
+        String sql = "SELECT * FROM CustomerContact WHERE CustomerContactID = ?";
+        return getTemplate().queryForObject(sql, new Object[] { id }, new CustomerContactWrapper());
+    }
+
+    @Override
+    public long create(CustomerContact m) {
+        Map<String, Object> attributes = new HashMap<>();
+
+        attributes.put("CustomerContactTypeID", m.getCustomerContactTypeId());
+        attributes.put("CustomerContactInfo", m.getCustomerContactInfo());
+        attributes.put("CustomerContactPrimary", m.isCustomerContactPrimary());
+
+        return this.getSimpleInsert().executeAndReturnKey(attributes).longValue();
+    }
+
+    @Override
+    public int update(long id, Map<String, Object> attributes) {
+        return this.update("CustomerContact", "CustomerContactID", id, attributes);
+    }
+
+    @Override
+    public int delete(long id) {
+        return this.delete("CustomerContact", "CustomerContactID", id);
+    }
 }
