b0a135a97adb25587bd2e677f6ae04a38ac604b8
jayjay-a <agno.julius@gmail.com>
2017-03-23 14:52:07 -0500

    fix typo

--- src/main/java/org/jsack/tifsa/Database/NoteType/NoteTypeWrapper.java
+++ src/main/java/org/jsack/tifsa/Database/NoteType/NoteTypeWrapper.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 /**
  * Created by julius on 3/23/2017.
  */
-public class NoteTypeWrapper implements org.springframework.jdbc.coreRowMapper<NoteType>{
+public class NoteTypeWrapper implements org.springframework.jdbc.core.RowMapper<NoteType>{
     public NoteType mapRow(ResultSet rs, int rowNum) throws SQLException {
         NoteType noteType = new NoteType();
 
