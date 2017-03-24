ca064d14a8d312caa3cdd024de3a48436919102e
Steve8139 <Steve8139@gmail.com>
2017-03-24 16:09:21 -0500

    Add files via upload

---
+++ OrderLineDeliveryType.java
@@ -0,0 +1,29 @@
+package org.jsack.tifsa.Database.OrderLineDeliveryType;
+
+/**
+ * Created by steven on 3/24/17.
+ */
+
+public class OrderLineDeliveryType {
+    private int orderLineDeliveryTypeId;
+    private String orderLineDeliveryTypeDescription;
+
+
+    public int getOrderLineDeliveryTypeId() {
+        return orderLineDeliveryTypeId;
+    }
+
+    public void setOrderLineDeliveryTypeId(int orderLineDeliveryTypeId) {
+        this.orderLineDeliveryTypeId = orderLineDeliveryTypeId;
+    }
+
+    public String getOrderLineDeliveryTypeDescription() {
+        return orderLineDeliveryTypeDescription;
+    }
+
+    public void setOrderLineDeliveryTypeDescription(String orderLineDeliveryTypeDescription) {
+        this.orderLineDeliveryTypeDescription = orderLineDeliveryTypeDescription;
+    }
+
+
+}
