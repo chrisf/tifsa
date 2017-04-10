b98df1bc6feb00bc1696dfd280bfe928e2747723
Steve8139 <steve8139@gmail.com>
2017-04-10 13:54:38 -0500

    made a few base files

---
+++ src/main/java/org/jsack/tifsa/Database/EmployeeRole/EmployeeRole.java
@@ -0,0 +1,26 @@
+package org.jsack.tifsa.Database.EmployeeRole;
+
+/**
+ * Created by steven on 4/10/17.
+ */
+
+public class EmployeeRole {
+  private Long employeeid;
+  private Long employeetypeid;
+
+  public Long getEmployeeid() {
+    return employeeid;
+  }
+
+  public void setEmployeeid(Long employeeid) {
+    this.employeeid = employeeid;
+  }
+
+  public Long getEmployeetypeid() {
+    return employeetypeid;
+  }
+
+  public void setEmployeetypeid(Long employeetypeid) {
+    this.employeetypeid = employeetypeid;
+  }
+}
---
+++ src/main/java/org/jsack/tifsa/Database/EmployeeStatus/EmployeeStatus.java
@@ -0,0 +1,26 @@
+package org.jsack.tifsa.Database.EmployeeStatus;
+
+/**
+ * Created by steven on 4/10/17.
+ */
+
+public class EmployeeStatus {
+  private Long employeestatusid;
+  private String employeestatusdescription;
+
+  public Long getEmployeestatusid() {
+    return employeestatusid;
+  }
+
+  public void setEmployeestatusid(Long employeestatusid) {
+    this.employeestatusid = employeestatusid;
+  }
+
+  public String getEmployeestatusdescription() {
+    return employeestatusdescription;
+  }
+
+  public void setEmployeestatusdescription(String employeestatusdescription) {
+    this.employeestatusdescription = employeestatusdescription;
+  }
+}
