85ca0ff7a2a5d6deb2262a75e97bb8b2e93c5b63
Steve8139 <steve8139@gmail.com>
2017-04-19 18:51:43 -0500

    added columns for sams reports.

---
+++ src/main/java/org/jsack/tifsa/Reports/Employee/EmployeeRoles/ReportModel.java
@@ -0,0 +1,42 @@
+package org.jsack.tifsa.Reports.Employee.EmployeeRoles;
+
+import org.jsack.tifsa.Reports.ColumnFormats.IntegerColumn;
+import org.jsack.tifsa.Reports.ColumnFormats.StringColumn;
+import org.jsack.tifsa.Reports.ColumnInfo;
+import org.jsack.tifsa.Reports.ReportModelBase;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * Created by aaron on 4/11/17.
+ */
+public class ReportModel extends ReportModelBase {
+
+    private List<String> row;
+    private LinkedHashMap<String, ColumnInfo> columns;
+
+    public ReportModel() {
+        row = new ArrayList<>();
+        columns = new LinkedHashMap<>();
+
+
+        columns.put("EmployeeID", new ColumnInfo("Employee ID", IntegerColumn.class));
+        columns.put("First Name", new ColumnInfo("First Name", StringColumn.class));
+        columns.put("Last Name", new ColumnInfo("Last Name", StringColumn.class));
+        columns.put("Position", new ColumnInfo("Position", StringColumn.class));
+        columns.put("EmployeeStatusDescription", new ColumnInfo("Employee Status", StringColumn.class));
+
+    }
+
+    @Override
+    public List<String> getRow() {
+        return row;
+    }
+
+    @Override
+    public LinkedHashMap<String, ColumnInfo> getColumns() {
+        return columns;
+    }
+}
