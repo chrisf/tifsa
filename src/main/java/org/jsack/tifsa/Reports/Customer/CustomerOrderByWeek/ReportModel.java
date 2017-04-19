58fb3480a1aad3e0e639ee7fceb2d544b2382363
Steve8139 <steve8139@gmail.com>
2017-04-19 13:40:13 -0500

    all of anils report columns added to ReportModels

---
+++ src/main/java/org/jsack/tifsa/Reports/Customer/CustomerOrderByWeek/ReportModel.java
@@ -0,0 +1,45 @@
+package org.jsack.tifsa.Reports.Customer.CustomerOrderByWeek;
+
+import org.jsack.tifsa.Reports.ColumnFormats.CurrencyColumn;
+import org.jsack.tifsa.Reports.ColumnFormats.StringColumn;
+import org.jsack.tifsa.Reports.ColumnInfo;
+import org.jsack.tifsa.Reports.ReportModelBase;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * Created by aaron on 4/11/17.
+ */
+public class ReportModel extends ReportModelBase {
+
+    private List<String> row;
+    private LinkedHashMap<String, ColumnInfo> columns;
+
+    public ReportModel() {
+        row = new ArrayList<>();
+        columns = new LinkedHashMap<>();
+
+
+
+        //Columns added by Steven
+        columns.put("Year", new ColumnInfo("Year", StringColumn.class));
+        columns.put("Month", new ColumnInfo("Month", StringColumn.class));
+        columns.put("CustomerFirst", new ColumnInfo("Customer First", StringColumn.class));
+        columns.put("CustomerLast", new ColumnInfo("Customer Last", StringColumn.class));
+        columns.put("ContactInfo", new ColumnInfo("Contact Info", StringColumn.class));
+        columns.put("OrderTotal", new ColumnInfo("Order Total", CurrencyColumn.class));
+        columns.put("OrderDiscount", new ColumnInfo("Order Discount", CurrencyColumn.class));
+    }
+
+    @Override
+    public List<String> getRow() {
+        return row;
+    }
+
+    @Override
+    public LinkedHashMap<String, ColumnInfo> getColumns() {
+        return columns;
+    }
+}
